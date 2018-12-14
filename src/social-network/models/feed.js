2c0ef7b10ac932da93ac545d00a53f9ee9107551
Ezio Ballarin <ezio.t.ballarin@gmail.com>
2018-12-14 20:01:40 +0000

    Got feed working

--- src/social-network/models/feed.js
+++ src/social-network/models/feed.js
@@ -3,22 +3,30 @@ var db = require('./db_connection');
 var conn = mysql.createConnection(db.config);
 
 exports.getFeed= function(params, callback) {
-	var query = 'SELECT * FROM subscriptions_user su, subscriptions_tag st WHERE su.user_id=?, st.user_id=?;';
+	var query = 'SELECT * FROM subscriptions_user su, subscriptions_tags st WHERE su.user_id=? AND st.user_id=?;';
+    console.log("got params to getFeed: ", params);
 	var queryData = [
-		params.sess.user,
-		params.sess.user
+		params.user,
+		params.user
 	];
+    console.log("running: ", query, queryData);
 	conn.query(query, queryData, function(err, result) {
-		console.log(query, queryData, result);
-		var content= [];
-		var query = 'SELECT * FROM content WHERE user_id = ? OR tag = ?;';
-		for(var i = 0; i < result.length; i++){
-			content.push([result[i].subscription_id, result[i].tag]);
-		}
-		conn.query(query, [content], function(err, result){
-			console.log(query, queryData, result);
-			callback(err, result);
-		});
+
+        if (err || result.length < 1) {
+            console.log("got err", err);
+            console.log("got error result", result);
+            callback(err, result);
+        } else {
+            console.log("got regular result: ", result);
+            var content= [];
+            var query = 'SELECT * FROM content WHERE user_id = ? OR tag = ?;';
+            for(var i = 0; i < result.length; i++){
+                content.push([result[i].subscription_id, result[i].tag]);
+            }
+            conn.query(query, [content], function(err, result){
+                console.log(query, queryData, result);
+                callback(err, result);
+            });
+        }
 	});
 };
-
--- src/social-network/routes/feed.js
+++ src/social-network/routes/feed.js
@@ -11,15 +11,11 @@ router.get('/', function(req, res) {
 		});
 	} else {
 		global.validateSession(req, res, function(req, res) {
-			var params = {
-				file: req.file,
-				body: req.body,
-				sess: req.session
-			};
-			feed.getFeed(params, function(err, result) {
+            console.log("sending params to getFeed model:", req.session);
+			feed.getFeed(req.session, function(err, result) {
 				if (err) {
 					console.log("Could not get feed:", err);
-					resredirect('/?getFeed=false');
+					res.redirect('/?getFeed=false');
 				} else {
 					res.render('feed/getFeed', {'result':result});
 				}
